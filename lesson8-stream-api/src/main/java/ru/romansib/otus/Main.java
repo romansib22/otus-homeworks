5cc67f4b5a8780dee5bb7f76293332257f723758
Roman Sibiryakov <sibiryakov@advantum.ru>
2024-10-02 10:09:09 +0300

    Работа над ошибками

--- lesson8-stream-api/src/main/java/ru/romansib/otus/Main.java
+++ lesson8-stream-api/src/main/java/ru/romansib/otus/Main.java
@@ -5,37 +5,37 @@ public class Main {
         TaskService taskService = new TaskService();
 
         System.out.println("Tasks with status NEW:");
-        System.out.println(taskService.getTasksByStatus(TaskStatus.NEW));
+        System.out.println(taskService.getTasksByStatus(TaskService.taskList.stream(), TaskStatus.NEW));
         System.out.println("Tasks with status IN_PROGRESS:");
-        System.out.println(taskService.getTasksByStatus(TaskStatus.IN_PROGRESS));
+        System.out.println(taskService.getTasksByStatus(TaskService.taskList.stream(), TaskStatus.IN_PROGRESS));
         System.out.println("Tasks with status CLOSED:");
-        System.out.println(taskService.getTasksByStatus(TaskStatus.CLOSED));
+        System.out.println(taskService.getTasksByStatus(TaskService.taskList.stream(), TaskStatus.CLOSED));
 
         System.out.println("-----------------------------");
 
         System.out.println("Check task with id 5 exists (exists):");
-        System.out.println(taskService.checkTaskExistsById(5));
+        System.out.println(taskService.checkTaskExistsById(TaskService.taskList.stream(), 5));
 
         System.out.println("Check task with id 50 exists (not exists):");
-        System.out.println(taskService.checkTaskExistsById(50));
+        System.out.println(taskService.checkTaskExistsById(TaskService.taskList.stream(), 50));
 
         System.out.println("-----------------------------");
         System.out.println("Sorted task list by status:");
-        System.out.println(taskService.getSortedTaskListByStatus());
+        System.out.println(taskService.getSortedTaskListByStatus(TaskService.taskList.stream()));
 
         System.out.println("Sorted task list by status second realization:");
-        System.out.println(taskService.getSortedTaskListByStatus2());
+        System.out.println(taskService.getSortedTaskListByStatus2(TaskService.taskList.stream()));
 
         System.out.println("-----------------------------");
         System.out.println("Number of tasks with status NEW:");
-        System.out.println(taskService.getCountTasksByStatus(TaskStatus.NEW));
+        System.out.println(taskService.getCountTasksByStatus(TaskService.taskList.stream(), TaskStatus.NEW));
         System.out.println("Number of tasks with status IN_PROGRESS:");
-        System.out.println(taskService.getCountTasksByStatus(TaskStatus.IN_PROGRESS));
+        System.out.println(taskService.getCountTasksByStatus(TaskService.taskList.stream(), TaskStatus.IN_PROGRESS));
         System.out.println("Number of tasks with status CLOSED:");
-        System.out.println(taskService.getCountTasksByStatus(TaskStatus.CLOSED));
+        System.out.println(taskService.getCountTasksByStatus(TaskService.taskList.stream(), TaskStatus.CLOSED));
 
         System.out.println("Number of tasks with status second realization:");
-        System.out.println(taskService.getCountTasksByStatus2());
+        System.out.println(taskService.getCountTasksByStatus2(TaskService.taskList.stream()));
     }
 
 
--- lesson8-stream-api/src/main/java/ru/romansib/otus/TaskService.java
+++ lesson8-stream-api/src/main/java/ru/romansib/otus/TaskService.java
@@ -1,7 +1,7 @@
 package ru.romansib.otus;
 
-import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -10,60 +10,56 @@ import java.util.stream.Stream;
 import static ru.romansib.otus.TaskStatus.*;
 
 public class TaskService {
+    public static final List<Task> taskList = List.of(new Task(1, "Task #1", CLOSED),
+            new Task(2, "Task #2", NEW),
+            new Task(3, "Task #3", IN_PROGRESS),
+            new Task(4, "Task #4", IN_PROGRESS),
+            new Task(5, "Task #5", CLOSED),
+            new Task(6, "Task #6", IN_PROGRESS),
+            new Task(7, "Task #7", NEW),
+            new Task(8, "Task #8", IN_PROGRESS),
+            new Task(9, "Task #9", NEW),
+            new Task(10, "Task #10", CLOSED));
 
-    public Stream<Task> getTaskStream() {
-        return Stream.of(new Task(1, "Task #1", CLOSED),
-                new Task(2, "Task #2", NEW),
-                new Task(3, "Task #3", IN_PROGRESS),
-                new Task(4, "Task #4", IN_PROGRESS),
-                new Task(5, "Task #5", CLOSED),
-                new Task(6, "Task #6", IN_PROGRESS),
-                new Task(7, "Task #7", NEW),
-                new Task(8, "Task #8", IN_PROGRESS),
-                new Task(9, "Task #9", NEW),
-                new Task(10, "Task #10", CLOSED));
-    }
+    public static List<TaskStatus> statusOrderList = List.of(NEW, IN_PROGRESS, CLOSED);
 
-    public List<Task> getTasksByStatus(TaskStatus status) {
-        Stream<Task> taskStream = getTaskStream();
+    public List<Task> getTasksByStatus(Stream<Task> taskStream, TaskStatus status) {
         //Получение списка задач по выбранному статусу;
         return taskStream
                 .filter(task -> task.getTaskStatus() == status)
                 .collect(Collectors.toList());
     }
 
-    public boolean checkTaskExistsById(int taskId) {
-        Stream<Task> taskStream = getTaskStream();
+    public boolean checkTaskExistsById(Stream<Task> taskStream, int taskId) {
         //Проверка наличия задачи с указанным ID;
         return taskStream.anyMatch(task -> task.getId() == taskId);
     }
 
-    public List<Task> getSortedTaskListByStatus() {
-        Stream<Task> taskStream = getTaskStream();
+    public List<Task> getSortedTaskListByStatus(Stream<Task> taskStream) {
         //Получение списка задач в отсортированном по статусу виде: открыта, в работе, закрыта
-        return taskStream.sorted(Comparator.comparing(Task::getTaskStatus)).collect(Collectors.toList());
+        //return taskStream.sorted(Comparator.comparing(Task::getTaskStatus)).collect(Collectors.toList());
+        return taskStream.sorted(new TaskComparator()).collect(Collectors.toList());
     }
 
-    public Map<TaskStatus, List<Task>> getSortedTaskListByStatus2() {
-        Stream<Task> taskStream = getTaskStream();
+
+    public Map<TaskStatus, List<Task>> getSortedTaskListByStatus2(Stream<Task> taskStream) {
         //Получение списка задач в отсортированном по статусу виде: открыта, в работе, закрыта
         return taskStream.collect(Collectors.groupingBy(Task::getTaskStatus, HashMap::new, Collectors.toList()));
     }
 
-    public int getCountTasksByStatus(TaskStatus status) {
-        Stream<Task> taskStream = getTaskStream();
+    public int getCountTasksByStatus(Stream<Task> taskStream, TaskStatus status) {
         //Подсчет количества задач по определенному статусу.
         return (int) taskStream.filter(task -> task.getTaskStatus() == status).count();
 
     }
 
-    public Map<TaskStatus, Integer> getCountTasksByStatus2() {
-        Stream<Task> taskStream = getTaskStream();
+    public Map<TaskStatus, Long> getCountTasksByStatus2(Stream<Task> taskStream) {
         //Подсчет количества задач по определенному статусу.
-        Map<TaskStatus, Integer> result = new HashMap<>();
-        taskStream.forEach(task -> {
-            result.put(task.getTaskStatus(), result.getOrDefault(task.getTaskStatus(),0) + 1);
-        });
+        //return taskStream.sorted(new TaskComparator()).collect(Collectors.groupingBy(Task::getTaskStatus, Collectors.counting()));
+        Map<TaskStatus, Long> statusCountMap = taskStream.collect(Collectors.groupingBy(Task::getTaskStatus, Collectors.counting()));
+        //Эксперимента ради, добавил сортировку по статусу.
+        Map<TaskStatus, Long> result = new LinkedHashMap<>();
+        statusCountMap.entrySet().stream().sorted(Map.Entry.comparingByKey(new TaskStatusComparator())).forEachOrdered(o -> result.put(o.getKey(), o.getValue()));
         return result;
     }
 }
