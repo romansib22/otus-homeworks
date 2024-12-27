c1214d8cb1b9241ce1c0b0e47a9252337374085e
Roman Sibiryakov <sibiryakov@advantum.ru>
2024-12-27 16:22:59 +0300

    Homework lesson 32 initial

---
+++ lesson32-rest-service/src/test/java/ru/otus/java/pro/spring/app/SpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.java.pro.spring.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
