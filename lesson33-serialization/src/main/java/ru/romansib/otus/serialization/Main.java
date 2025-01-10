de80d43ed26e4a40897ba7493fad4bc9f2372e93
Roman Sibiryakov <sibiryakov@advantum.ru>
2025-01-10 13:50:49 +0300

    lesson33 serialization init

---
+++ lesson33-serialization/src/main/java/ru/romansib/otus/serialization/Main.java
@@ -0,0 +1,11 @@
+package ru.romansib.otus.serialization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
